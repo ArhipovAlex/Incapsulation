0403f72db4ffba9b6cfc33fe3f74d2a955dcf34d
Sania <Sania@DESKTOP-3HL58UN>
2023-11-14 21:20:38 +0800

    Add project files.

---
+++ Point/Point.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    internal class Point
+    {
+        double x;
+        double y;
+        public double GetX()
+        {
+            return x; 
+        }
+        public double GetY() 
+        {
+            return y;
+        }
+        public void SetX(double x) 
+        {
+            this.x = x;
+        }
+        public void SetY(double y) 
+        {
+            this.y = y;
+        }
+
+        //          Properties:
+        public double X
+        { 
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        //          Methods:
+        public void Print()
+        {
+            Console.WriteLine($"X={x},\tY={y}");
+        }
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point A=new Point();
+            A.SetX(2);
+            A.SetY(3);
+            A.Print();
+
+            A.X = 22;
+            A.Y = 33;
+            A.Print();
+        }
+    }
+}
